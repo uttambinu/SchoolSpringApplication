643472c3a6325bfb8d3468c308aa5b3b768df9c8
uttambinu <uttamprakash2307@gmail.com>
2022-06-20 13:46:44 +0530

    Enum created for Gender

---
+++ school/src/main/java/com/school/schoolDemo/entity/enums/Gender.java
@@ -0,0 +1,12 @@
+package com.school.schoolDemo.entity.enums;
+
+public enum Gender {
+    MALE("male"),
+    FEMALE("female"),
+    OTHERS("others");
+
+    private String code;
+
+    Gender(String code) {
+    }
+}
